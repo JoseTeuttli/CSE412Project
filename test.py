f6b3258b94528d745874aa6b49b20519e640a46f
JoseTeuttli <jose.gerardo.teuttli@gmail.com>
2021-11-27 12:43:39 -0700

    added ability to add songs to playlists
    Co-authored-by: skaruna2 <skaruna2@users.noreply.github.com>

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ conn = psycopg2.connect(host="localhost", port=5432,
                         database="postgres", user="postgres", password="password")
 cur = conn.cursor()
 
-#global PROFILE_ID
+# global PROFILE_ID
 profile_id = -1
-#global PROFILE_USERNAME
+# global PROFILE_USERNAME
 profile_username = ""
 
 
@@ -40,6 +40,14 @@ def search_result():
     search_text = request.form['search_text']
     search_type = str(request.form['search_type'])
     if(search_type == 'album'):
+        # cur.execute("""select song_name,album.name,artist.name,song.date_created,song_is_genre.name from song,album,artist,song_part_of_album,song_made_artist,song_is_genre,album_made_artist
+        #             where album.albumid=album_made_artist.albumid
+        #             and artist.artistid=album_made_artist.artistid
+        #             and song.songid=song_part_of_album.songid
+        #             and album.albumid=song_part_of_album.albumid
+        #             and song.songid=song_made_artist.songid
+        #             and artist.artistid=song_made_artist.artistid
+        #             and song.songid=song_is_genre.songid""")
         cur.execute("""select song_name,album.name,artist.name,song.date_created,song_is_genre.name from song,album,artist,song_part_of_album,song_made_artist,song_is_genre,album_made_artist
                     where album.albumid=album_made_artist.albumid
                     and artist.artistid=album_made_artist.artistid
@@ -79,20 +87,35 @@ def search_result():
                     and artist.artistid=song_made_artist.artistid
                     and song.songid=song_is_genre.songid
                     and song_is_genre.name=%s""", (search_text,))
-    query_results = cur.fetchall()
+    song_results = cur.fetchall()
+    # data_tuples = []
+    # for query_result in query_results:
+    #     print(str(query_result[0]).replace('\'', ''))
+    #     data_tuple = (str(query_result[0]).replace('\'', ''), str(query_result[1]).replace(
+    #         '\'', ''), str(query_result[2]).replace('\'', ''), query_result[3], str(query_result[4]).replace('\'', ''))
+    #     data_tuples.append(data_tuple)
+    # print(data_tuples)
+    if profile_id == -1:
+        print(profile_id)
+        return render_template('search_result.html',
+                               data=song_results, user_id=profile_id)
+    cur.execute("""select playlist_name from profile,profile_made_playlist,playlist
+                where profile.profileid=profile_made_playlist.profileid
+                and playlist.playlistid=profile_made_playlist.playlistid
+                and profile.username=%s""", (profile_username,))
+    playlist_results = cur.fetchall()
+    return render_template('search_result.html', data=song_results, user_id=profile_id, playlists=playlist_results)
 
-    return render_template('search_result.html', data=query_results)
 
-
-@app.route('/login')
+@ app.route('/login')
 def login():
     return render_template('login.html')
 
 
-@app.route('/logged_in', methods=['POST'])
+@ app.route('/logged_in', methods=['POST'])
 def logged_in():
     username = request.form['username']
-    cur.execute("""select profileid from profile 
+    cur.execute("""select profileid from profile
                 where profile.username=%s""", (username,))
     query_results = cur.fetchall()
     global profile_username
@@ -111,7 +134,7 @@ def logged_in():
     return render_template('logged_in.html', profile_username=profile_username)
 
 
-@app.route('/playlist')
+@ app.route('/playlist')
 def playlist():
     if profile_id == -1:
         return render_template('login.html')
@@ -143,14 +166,14 @@ def playlist():
     return render_template('playlist.html', playlists=playlist_data)
 
 
-@app.route('/new_playlist')
+@ app.route('/new_playlist')
 def new_playlist():
     if profile_id == -1:
         return render_template('login.html')
     return render_template('new_playlist.html')
 
 
-@app.route('/create_playlist', methods=['POST'])
+@ app.route('/create_playlist', methods=['POST'])
 def create_playlist():
     playlist_name = request.form['playlist_name']
     cur.execute("""select * from playlist, profile, profile_made_playlist
@@ -173,6 +196,27 @@ def create_playlist():
         return render_template('create_playlist.html', playlist_name=playlist_name)
     else:
         return render_template('playlist.html')
+
+
+@ app.route('/add_playlist', methods=['POST'])
+def add_playlist():
+    song_name = request.form['song_name']
+    playlist_name = request.form['playlist_name']
+    if profile_id == -1:
+        return render_template('login.html')
+    cur.execute("""select playlist.playlistid from playlist, profile, profile_made_playlist
+                where profile.profileid=profile_made_playlist.profileid
+                and playlist.playlistid=profile_made_playlist.playlistid
+                and playlist.playlist_name=%s""", (playlist_name,))
+    add_playlist_id = cur.fetchall()[0]
+    cur.execute("""select song.songid from song
+                where song.song_name=%s""", (song_name,))
+    add_song_id = cur.fetchall()[0]
+    cur.execute("""insert into part_of_playlist values (%s, %s)""",
+                (add_playlist_id, add_song_id))
+    conn.commit()
+
+    return render_template('success.html', data=song_name+playlist_name)
 
 
 if __name__ == '__main__':  # python interpreter assigns "__main__" to the file you run
