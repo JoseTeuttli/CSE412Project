8370d53b10eca31f0a8a4ff1df566b02e6780d85
Tyler C <trchang@asu.edu>
2021-11-28 12:26:44 -0800

    Started formatting and beautification

--- test.py
+++ test.py
@@ -39,14 +39,6 @@ def search_result():
     search_text = request.form['search_text']
     search_type = str(request.form['search_type'])
     if(search_type == 'album'):
-        # cur.execute("""select song_name,album.name,artist.name,song.date_created,song_is_genre.name from song,album,artist,song_part_of_album,song_made_artist,song_is_genre,album_made_artist
-        #             where album.albumid=album_made_artist.albumid
-        #             and artist.artistid=album_made_artist.artistid
-        #             and song.songid=song_part_of_album.songid
-        #             and album.albumid=song_part_of_album.albumid
-        #             and song.songid=song_made_artist.songid
-        #             and artist.artistid=song_made_artist.artistid
-        #             and song.songid=song_is_genre.songid""")
         cur.execute("""select song_name,album.name,artist.name,song.date_created,song_is_genre.name from song,album,artist,song_part_of_album,song_made_artist,song_is_genre,album_made_artist
                     where album.albumid=album_made_artist.albumid
                     and artist.artistid=album_made_artist.artistid
